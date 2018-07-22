61fccf8c2fd6c8a3b5e136524e416d893fc4474c
Andy <458666482@qq.com>
2018-07-22 15:02:10 +0800

    163

--- ScrollTop/js/main.js
+++ ScrollTop/js/main.js
@@ -1,19 +1,33 @@
-requirejs(['jquery'],function($){
-    $('#backTop').on('click',move);
-    $(window).on('scroll',function(){
-        checkPosition($(window).height())
+requirejs(['jquery','backtop'],function($,backtop){
+    $('#backTop').backtop({
+        mode:'move'
     });
-    checkPosition($(window).height());
-    function move(){
-        $('html,body').animate({
-            scrollTop:0
-        },800);
-    }
-    function checkPosition(pos) {
-        if($(window).scrollTop()>pos){
-            $('#backTop').fadeIn();
-        }else{
-            $('#backTop').fadeOut();
-        }
-    }
-})
+
+    // new backtop.BackTop($('#backTop'),{
+    //     mode:'move'
+    // });
+
+
+
+    // var scroll = new scrollTo.ScrollTo({
+    //     dest:0,
+    //     speed:5000
+    // });
+    // $('#backTop').on('click',$.proxy(scroll.move,scroll));
+    // $(window).on('scroll',function(){
+    //     checkPosition($(window).height())
+    // });
+    // checkPosition($(window).height());
+    // function move(){
+    //     $('html,body').animate({
+    //         scrollTop:0
+    //     },800);
+    // }
+    // function checkPosition(pos) {
+    //     if($(window).scrollTop()>pos){
+    //         $('#backTop').fadeIn();
+    //     }else{
+    //         $('#backTop').fadeOut();
+    //     }
+    // }
+});
